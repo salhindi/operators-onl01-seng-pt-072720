4d4ccf37430fba145c8e87350ff083f2cf464baa
Sara A <calisaraalhindi@gmail.com>
2020-07-31 21:14:38 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,11 +12,10 @@ end
 
 def not_safe?(speed)
   speed > 60 ? true : false
-  elsif speed < 40
+  speed < 40
     true
-  elsif speed > 40 && speed < 60
+  speed > 40 && speed < 60
   false
-    end
 end
 	
 
